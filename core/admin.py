c85213536b35208380f83faea4615b1a545be68c
AnsarIKz <nansarik@mail.ru>
2023-03-17 01:39:31 +0600

    Admin Reg

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
-
+from .models import PostPhoto, Post, UsefulPhoto, Useful, Request, Category, Review, Project, ProjectPhoto
 # Register your models here.
+
+
+class PostPhotoInline(admin.TabularInline):
+    model = PostPhoto
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [PostPhotoInline]
+
+
+class UsefulPhotoInline(admin.TabularInline):
+    model = UsefulPhoto
+
+
+class UsefulAdmin(admin.ModelAdmin):
+    inlines = [UsefulPhotoInline]
+
+
+class ProjectPhotoInline(admin.TabularInline):
+    model = ProjectPhoto
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    inlines = [ProjectPhotoInline]
+
+
+# Images
+admin.site.register(Post, PostAdmin)
+admin.site.register(Useful, UsefulAdmin)
+
+admin.site.register(Request)
+admin.site.register(Category)
+admin.site.register(Review)
